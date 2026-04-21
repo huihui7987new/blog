eabacef41622e51f9f9b6c75743857e62a6508d3
huihui7987new <huihui7987@petalmail.com>
2026-04-21 23:11:12 +0800

    deploy: registerSW.js

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/blog/sw.js', { scope: '/blog/' })})}
